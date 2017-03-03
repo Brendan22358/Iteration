28b58735b176ed8aae8c38f691905b057d5e4887
Brendan22358 <bgallagher@sjnma.org>
2017-03-03 10:00:26 -0600

    [problem2 is now definitley done.]

--- problem2.py
+++ problem2.py
@@ -1,17 +1,15 @@
 from random import randrange
-ctr=1
 numlow=1
 numhigh=1001
-secret=int(raw_input("We are going to play high or lower again but this time the computer is guessing!\nInput a secret number from 1 to 1000\n>"))
+highorlow=" "
+print("We are going to play high or lower again but this time the computer is guessing!\nThink of a secret number from 1 to 1000")
 numberguess=randrange(1,1000)
-if(secret<1 or secret>1000):
-	secret=int(raw_input("Error 403: Enter a number between 1 and 1000!!!\n>"))
-while(secret!=numberguess):
-	highorlow=raw_input("The computer guessed {}, is that too high or too low?\n>".format(numberguess))
-	if(highorlow=="high" and ctr==1):
+while(highorlow!="right"):
+	highorlow=raw_input("The computer guessed {}, is that too high or too low, or just right?\n>".format(numberguess))
+	if(highorlow=="high"):
 		numhigh=numberguess-1
 		numberguess=randrange(numlow,numhigh)
-	elif(highorlow=="low" and ctr==1):
+	elif(highorlow=="low"):
 		numlow=numberguess+1
 		numberguess=randrange(numlow,numhigh)
-print("Congrats the computer got it!\nComputer - The number is (Machine clinking) {}.".format(secret))
+print("Congrats the computer got it!\nComputer - The number is (Machine clinking) {}.".format(numberguess))
