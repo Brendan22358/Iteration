a8cd02fa050cd33fb2b435380f0f421410053172
Brendan22358 <bgallagher@sjnma.org>
2017-03-07 10:23:40 -0600

    [Bug fixed]

--- problem2.py
+++ problem2.py
@@ -7,9 +7,9 @@ numberguess=randrange(1,1000)
 while(highorlow!="right"):
 	highorlow=raw_input("The computer guessed {}, is that too high or too low, or just right?\n>".format(numberguess))
 	if(highorlow=="high"):
-		numhigh=numberguess-1
+		numhigh=numberguess
 		numberguess=randrange(numlow,numhigh)
 	elif(highorlow=="low"):
-		numlow=numberguess+1
+		numlow=numberguess
 		numberguess=randrange(numlow,numhigh)
 print("Congrats the computer got it!\nComputer - The number is (Machine clinking) {}.".format(numberguess))
